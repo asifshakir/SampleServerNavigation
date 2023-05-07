9fb60ee7e70409812aec08c6ca93a21066d4d6e3
Asif Shakir <asif.shakir@gmail.com>
2023-05-07 21:18:24 +0530

    Add project files.

---
+++ SampleServer/Shared/CustomNavigationManager.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace SampleServer.Shared;
+
+public class CustomNavigationManager : NavigationManager
+{
+    protected override void NavigateToCore(string uri, bool forceLoad)
+    {
+        var options = new NavigationOptions {
+            ForceLoad = forceLoad,
+            ReplaceHistoryEntry = false
+        };
+        base.NavigateToCore(uri, options);
+    }
+}
